af3281fbf9d38f1e74c679ef1d98a3f2d2f9317c
Robin Tapiru <robintapiru0894@gmail.com>
2023-09-11 12:25:16 +0400

    on going

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,21 @@
-$("#menu").click(function () {
-  $(this).next().addClass("show");
+//  AOS
+AOS.init();
+//SPLIDE
+var splide = new Splide(".splide", {
+  type: "loop",
+  pagination: false,
 });
-
+splide.mount();
+//NAV
+$("#menu").click(function () {
+  $("nav ul").addClass("show");
+  $("body").addClass("disable");
+});
+$(".close-menu").click(function () {
+  $("body").removeClass("disable");
+  $("nav ul.show").removeClass("show");
+});
 $(window).resize(function () {
   $("nav ul.show").removeClass("show");
+  $("body").removeClass("disable");
 });
