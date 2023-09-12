f36a26292d4a0477baf7e7d961ca699d99f232d8
Robin Tapiru <robintapiru0894@gmail.com>
2023-09-12 18:40:55 +0400

    homepage donw

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,28 @@ $(window).resize(function () {
   $("nav ul.show").removeClass("show");
   $("body").removeClass("disable");
 });
+
+$(document).ready(function () {
+  // Open the first item by default
+  $(".accordion li:first-child .accordion-content").show();
+
+  // Toggle accordion items on click
+  $(".accordion-header").click(function () {
+    var content = $(this).next(".accordion-content");
+
+    // Check if the clicked header is active
+    if ($(this).hasClass("active")) {
+      content.slideUp();
+      $(this).removeClass("active");
+    } else {
+      // Close all accordion items except the one clicked
+      $(".accordion-content").not(content).slideUp();
+      // Remove "active" class from all headers
+      $(".accordion-header").removeClass("active");
+
+      // Toggle the clicked item
+      content.slideDown();
+      $(this).addClass("active");
+    }
+  });
+});
